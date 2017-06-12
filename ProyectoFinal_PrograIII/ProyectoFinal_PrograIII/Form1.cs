3dd88e64339eba533e8e179568903b8786bc49e3
Hiuwini <hiuwini.17@gmail.com>
2017-06-12 13:10:07 -0600

    Lectura de el archivo de texto comunidades

--- ProyectoFinal_PrograIII/ProyectoFinal_PrograIII/Form1.cs
+++ ProyectoFinal_PrograIII/ProyectoFinal_PrograIII/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ProyectoFinal_PrograIII
 {
@@ -16,6 +17,32 @@ namespace ProyectoFinal_PrograIII
         {
             InitializeComponent();
         }
-        //Esto es un comentario
+
+        List<Comunidad> Comunidades = new List<Comunidad>();
+
+        void LeerComunidad()
+        {
+            string fileName = @"C:\Users\Hiuwini\Source\Repos\Proyecto_Final\ProyectoFinal_PrograIII\ProyectoFinal_PrograIII\obj\Comunidades.txt";
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                Comunidad temp = new Comunidad();
+                temp.Codigo_comunidad = Convert.ToInt32(reader.ReadLine());
+                temp.Nombre_comunidad = reader.ReadLine();
+                temp.Poblacion = reader.ReadLine();
+                Comunidades.Add(temp);
+            }
+
+            reader.Close();
+
+           // dataGridView1.DataSource = Comunidades;
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LeerComunidad();
+        }
     }
 }
