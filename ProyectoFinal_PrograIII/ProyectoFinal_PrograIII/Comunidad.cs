b945fa198e3642cdbc95568d319823e07799e4b3
Hiuwini <hiuwini.17@gmail.com>
2017-06-12 13:09:40 -0600

    Clase Comunidad --- Aqui se almacenaran las comunidades

---
+++ ProyectoFinal_PrograIII/ProyectoFinal_PrograIII/Comunidad.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_PrograIII
+{
+    class Comunidad
+    {
+        int codigo_comunidad;
+        string nombre_comunidad;
+        string poblacion;
+
+        public int Codigo_comunidad
+        {
+            get
+            {
+                return codigo_comunidad;
+            }
+
+            set
+            {
+                codigo_comunidad = value;
+            }
+        }
+
+        public string Nombre_comunidad
+        {
+            get
+            {
+                return nombre_comunidad;
+            }
+
+            set
+            {
+                nombre_comunidad = value;
+            }
+        }
+
+        public string Poblacion
+        {
+            get
+            {
+                return poblacion;
+            }
+
+            set
+            {
+                poblacion = value;
+            }
+        }
+    }
+}
